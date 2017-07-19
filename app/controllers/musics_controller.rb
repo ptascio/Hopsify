d3a943153b9e07eed57e81aabd5f51256107c76b
Peter Tascio <peter.tascio@gmail.com>
2017-07-19 17:08:10 -0400

    Write new access token method

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -2,4 +2,10 @@ class MusicsController < ApplicationController
   def index
     render 'index'
   end
+
+  def get_new_access_token
+  end
+
+  def hit_spotify
+  end
 end
--- app/models/music.rb
+++ app/models/music.rb
@@ -1,8 +1,24 @@
 class Music
    include ActiveModel::Model
+   require 'rest-client'
+   require 'base64'
 
    attr_accessor :artist
    attr_accessor :album
    attr_accessor :track
+
+  def self.get_new_access_token
+    new_token = RestClient::Request.execute(
+      method: :post,
+      url: 'https://accounts.spotify.com/api/token',
+      payload: {'grant_type': 'client_credentials'},
+      headers: {
+        Authorization: ENV["hashed_auth"]
+      }
+    )
+    puts new_token.body
+
+
+  end
 
 end

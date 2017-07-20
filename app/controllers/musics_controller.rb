c9e010d1599513031730cdaca20f366f9cfbfc27
Peter Tascio <peter.tascio@gmail.com>
2017-07-20 16:13:57 -0400

    Validation errors for form

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -3,20 +3,28 @@ class MusicsController < ApplicationController
   attr_accessor :artist_details
   attr_accessor :artist_name
   attr_accessor :track_name
+  attr_accessor :music
   def index
+    @music = Music.new
     render 'index'
   end
 
   def create
     artist = params[:music][:artist]
     track = params[:music][:track]
-    self.artist_name = artist
-    self.track_name = track
-    all_music_details = Music.set_params(artist, track)
-    self.track_details = all_music_details[0]
-    self.artist_details = all_music_details[1]
-    debugger
-    render 'show'
+    if ((artist.length < 1) || (track.length < 1))
+      @music = Music.new
+      flash[:error] = "Both Fields Must Be Filled In!"
+      render 'index'
+    else
+      self.artist_name = artist
+      self.track_name = track
+      all_music_details = Music.set_params(artist, track)
+      self.track_details = all_music_details[0]
+      self.artist_details = all_music_details[1]
+      render 'show'
+    end
+
   end
 
   def show

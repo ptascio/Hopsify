0683c6d27e34555a07f8ef2079057a5782de794b
Peter Tascio <peter.tascio@gmail.com>
2017-07-21 14:18:36 -0400

    Specify error

--- app/models/music.rb
+++ app/models/music.rb
@@ -50,7 +50,7 @@ class Music
     song_info = JSON.parse(song_info.body)
     if song_info["tracks"]["items"].empty?
       @all_info.push({"error"=>"We couldn't find that song. Are you sure that the artist and track
-        names are spelled correctly? Keep in mind that not all artists and songs
+        names are spelled correctly? *Keep in mind that not all artists and songs
         are available on the Spotify platform."})
         return @all_info
     else

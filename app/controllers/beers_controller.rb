08edde6a46d794b532a84bf3207958e914c6c35f
Peter Tascio <peter.tascio@gmail.com>
2017-07-22 08:01:23 -0400

    Write method to get beer based on name

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,2 +1,11 @@
-class BeersController
+class BeersController < ApplicationController
+  def index
+    @beer = Beer.get_beer_info
+    render 'index'
+  end
+
+  def create
+
+    render "show"
+  end
 end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,4 +3,17 @@ class Beer
    require 'rest-client'
    require 'base64'
    require 'uri'
+
+   def self.get_beer_info
+     uri = URI.parse('http://api.brewerydb.com/v2/beers?')
+     params = URI.decode_www_form(uri.query)
+     params << ['key', ENV["brewery_key"]]
+     params << ['name', "Budweiser"]
+     uri.query = URI.encode_www_form(params)
+     selected_beer = RestClient::Request.execute(
+      method: :get,
+      url: "#{uri}",
+     )
+     return JSON.parse(selected_beer.body)
+   end
 end

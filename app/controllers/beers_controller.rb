cc19d70af0b04629023a4f8d801ffb4caa6ecc5c
Peter Tascio <peter.tascio@gmail.com>
2017-07-22 08:31:00 -0400

    Get beers by styleId

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,6 +1,7 @@
 class BeersController < ApplicationController
   def index
-    @beer = Beer.get_beer_info
+    @beer = Beer.get_beer_by_style
+    debugger
     render 'index'
   end
 
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -16,4 +16,21 @@ class Beer
      )
      return JSON.parse(selected_beer.body)
    end
+
+   def self.get_beer_by_style
+     uri = URI.parse('http://api.brewerydb.com/v2/beers?')
+     params = URI.decode_www_form(uri.query)
+     params << ['key', ENV["brewery_key"]]
+     params << ['styleId', "93"]
+     params << ['randomCount', "3"]
+     uri.query = URI.encode_www_form(params)
+     debugger
+     selected_beer = RestClient::Request.execute(
+      method: :get,
+      url: "#{uri}",
+     )
+
+     return JSON.parse(selected_beer.body)
+   end
+
 end
